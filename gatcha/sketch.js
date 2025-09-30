4bbdc8c88dfeab7d6d2e9c881a181eed61eeae61
sooyeonC <162096004+sooyeonC@users.noreply.github.com>
2025-09-30 15:12:57 +0900

    time color

--- gatcha/sketch.js
+++ gatcha/sketch.js
@@ -1,29 +1,39 @@
 let body;
 let face;
+let isDaytime = false;
 
 function preload() {
   body = loadImage('gatcha-images/body.png');
-   let n = int(random(1, 5));
-   let m = int(random(0, 256));
-   let imgName = n + ".png";
-   face = loadImage("gatcha-images/" + imgName);
+  let n = int(random(1, 5));
+  let m = int(random(0, 256));
+  let imgName = n + ".png";
+  face = loadImage("gatcha-images/" + imgName);
 }
 
 function setup() {
   createCanvas(400, 400);
-   let button = createButton('🤗');
+  let button = createButton('🤗');
   button.position(10, 10);
 
   button.mousePressed(() => {
-   let n = int(random(1, 5));
-   let imgName = n + ".png";
-   face = loadImage("gatcha-images/" + imgName);
-  });  
+    let n = int(random(1, 5));
+    let imgName = n + ".png";
+    face = loadImage("gatcha-images/" + imgName);
+
+    const currentHour = hour();
+    isDaytime = (currentHour >= 7 && currentHour < 19);
+  });
 }
 
 function draw() {
-    background('beige');
+  background('beige');
   image(body, 0, 0);
   image(face, 132, 134);
+
+  if (isDaytime == true) {
+    background('#fcf9d2ff');
+  } else {
+    background('#000022');
+  }
 
 }
--- moving pattern/sketch.js
+++ moving pattern/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 function draw() {
   background(255, 255, 220);
   const cz = random(60, CELL);
-  const sz = random(60, CELL+20);
+  const sz = random(60, CELL + 20);
 
   for (let gy = 0; gy < GRID; gy++) {
     for (let gx = 0; gx < GRID; gx++) {
@@ -30,6 +30,14 @@ function draw() {
       translate(cx, cy);
       fill('lightblue');
       circle(0, 0, sz);
+      pop();
+      push();
+      translate(width / 2, height / 2); // move origin to canvas center
+      rotate(random(TWO_PI));       // random rotation
+      const s = random(300, 500);   // side length
+      const h = (sqrt(3) / 2) * s;  // height of equilateral triangle
+      fill('cyan');
+      triangle(-s / 2, h / 3, s / 2, h / 3, 0, -2 * h / 3);
       pop();
     }
   }
