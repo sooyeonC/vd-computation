815089162e7796a68fedb7d1e7d9a053e4370d04
sooyeonC <162096004+sooyeonC@users.noreply.github.com>
2025-09-30 12:05:19 +0900

    finished project

--- gatcha/sketch.js
+++ gatcha/sketch.js
@@ -3,14 +3,27 @@ let face;
 
 function preload() {
   body = loadImage('gatcha-images/body.png');
-  face = loadImage('gatcha-images/1.png');
+   let n = int(random(1, 5));
+   let m = int(random(0, 256));
+   let imgName = n + ".png";
+   face = loadImage("gatcha-images/" + imgName);
 }
 
 function setup() {
   createCanvas(400, 400);
+   let button = createButton('🤗');
+  button.position(10, 10);
+
+  button.mousePressed(() => {
+   let n = int(random(1, 5));
+   let imgName = n + ".png";
+   face = loadImage("gatcha-images/" + imgName);
+  });  
 }
 
 function draw() {
-  background(220);
+    background('beige');
   image(body, 0, 0);
+  image(face, 132, 134);
+
 }
