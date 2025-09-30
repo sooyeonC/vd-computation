4c72e401f41fc41eecf68bc8cb52442669829170
sooyeonC <162096004+sooyeonC@users.noreply.github.com>
2025-09-30 15:05:59 +0900

    finished making basic pattern

--- moving pattern/sketch.js
+++ moving pattern/sketch.js
@@ -1,31 +1,42 @@
-const GRID = 2;
+const GRID = 16;
 const CELL = 1080 / GRID;
 
 
 function setup() {
   createCanvas(1080, 1080);
   noStroke();
-  noLoop();
+  //noLoop();
+  frameRate(6);
 }
 
 function draw() {
   background(255, 255, 220);
+  const cz = random(60, CELL);
+  const sz = random(60, CELL+20);
 
-    for (let gy = 0; gy < GRID; gy++) {
-    const cy = gy * CELL + CELL / 2;
+  for (let gy = 0; gy < GRID; gy++) {
+    for (let gx = 0; gx < GRID; gx++) {
+      const cx = gx * CELL + CELL / 2;
+      const cy = gy * CELL + CELL / 2;
 
-  push();
-  fill('pink');
-  rectMode(CENTER);
-  rect(cy, cy, 380, 380);
-  pop();
+      push();
+      fill('pink');
+      rectMode(CENTER);
+      rect(cx, cy, cz, cz);
+      pop();
 
-  push();
-  blendMode(MULTIPLY);
-  translate(cy, cy);
-  fill('lightblue');
-  const sz = random(300, 500);
-  circle(0, 0, sz);
-  pop();
+      push();
+      blendMode(MULTIPLY);
+      translate(cx, cy);
+      fill('lightblue');
+      circle(0, 0, sz);
+      pop();
+    }
+  }
 }
+
+function keyPressed() {
+  if (key === 's') {
+    saveGif('mySketch', 5);
+  }
 }
