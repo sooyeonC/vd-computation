d0d541fb1a953ffebbbf286f201edbb790bf0ea2
sooyeonC <162096004+sooyeonC@users.noreply.github.com>
2025-09-30 11:46:28 +0900

    adding texts in sketch js

--- gatcha/sketch.js
+++ gatcha/sketch.js
@@ -1,10 +1,16 @@
 let body;
 let face;
 
+function preload() {
+  body = loadImage('body.png');
+  face = loadImage('1.png');
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  image(body, 0, 0);
 }
